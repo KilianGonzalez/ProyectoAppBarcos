94bb84a6d4ed07ffb8ea8a6b0dc2627d435c2e6b
Edgar Luaces Roig <edgar_luaces@ceroca.cat>
2025-01-29 15:10:31 +0100

    main--> iniciar pagina

---
+++ lib/pages/paginaprincipal.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Paginaprincipal extends StatefulWidget {
+  const Paginaprincipal({super.key});
+
+  @override
+  State<Paginaprincipal> createState() => _PaginaprincipalState();
+}
+
+class _PaginaprincipalState extends State<Paginaprincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
